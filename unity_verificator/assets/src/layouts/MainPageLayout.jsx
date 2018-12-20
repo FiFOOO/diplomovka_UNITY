a05fbc2fbdc36949d79137e8ddfe6fa57e7be03c
FiFOOO <splinter1231@gmail.com>
2018-12-20 12:50:56 +0100

    add react

---
+++ unity_verificator/assets/src/layouts/MainPageLayout.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class MainPageLayout extends Component {
+  render() {
+    return <div>Testing</div>;
+  }
+}
---
+++ unity_verificator/assets/src/pages/mainPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainPageLayout from '../layouts/MainPageLayout';
+
+ReactDOM.render(<MainPageLayout />, document.getElementById('content'));
